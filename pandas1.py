0d89c8974861f4db635063dc1e28107d327cfa1f
Faiq Ahmad <faiqahmad05252gmail.com>
2026-01-10 06:06:38 +0500

    working

--- pandas1.py
+++ pandas1.py
@@ -1,5 +1,11 @@
+
 import numpy as np
 import pandas as pd 
-path = r"local-repo\audi.csv"
+path = r"C:\Users\ADMIN\OneDrive\Desktop\git-demo\local-repo\audi.csv"
 df = pd.read_csv(path)
-print(df.head())
+#print(df.head())
+#print(df.info())
+#print(df.shape)
+#print(df.columns)
+#print(df.dtypes)
+print(df.describe())
